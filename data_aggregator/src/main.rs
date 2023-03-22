08cb12e1d64c6c332083919d4a503598c92555b0
aliraad79 <a.ahmadi.k.79@gmail.com>
2023-03-22 09:28:05 +0330

    Init data aggregator

--- data_aggregator/src/main.rs
+++ data_aggregator/src/main.rs
@@ -1,4 +1,19 @@
 mod rediss;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Task<'r> {
+    title: &'r str,
+    description: &'r str,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // let tasks_string = rediss::get_tasks();
+
+    // let tasks: Vec<Task> = tasks_string
+    //     .into_iter()
+    //     .map(|f| serde_json::from_str::<Task>(f.clone().as_str()).expect("Exec"))
+    //     .collect::<Vec<Task>>();
+    // println!("{:?}", tasks);
 }
--- data_aggregator/src/rediss.rs
+++ data_aggregator/src/rediss.rs
@@ -1,17 +1,33 @@
 extern crate redis;
 
-pub fn get_tasks() {
+use crate::Task;
+
+pub fn get_tasks() -> Vec<String> {
     let client = redis::Client::open("redis://127.0.0.1:6379").unwrap();
     let mut conn = client.get_connection().unwrap();
-    // let task_string = convert_task_to_string(task);
-    // con.LPUSH
-    let _: () = redis::cmd("LRANGE")
+
+    let keys_length: usize = redis::cmd("LLEN")
         .arg("tasks")
-        .arg("-1 0")
         .query(&mut conn)
-        .expect("failed to execute LPUSH for 'items'");
+        .expect("failed to execute LLEN for 'tasks'");
+
+    let items: Vec<String> = redis::cmd("RPOP")
+        .arg("tasks")
+        .arg(keys_length)
+        .query(&mut conn)
+        .expect("failed to execute RPOP for 'tasks'");
+
+    return items;
 }
 
-// fn convert_task_to_string(task: Task) -> String {
-//     format!("{} {}", task.title, task.description)
-// }
+pub fn get_last_task() -> Task<'static> {
+    let client = redis::Client::open("redis://127.0.0.1:6379").unwrap();
+    let mut conn = client.get_connection().unwrap();
+
+    let bar: String = redis::cmd("GET")
+        .arg("tasks")
+        .query(&mut conn)
+        .expect("failed to execute GET for 'tasks'");
+
+    serde_json::from_str::<Task>(bar.as_str()).expect("Exec")
+}
