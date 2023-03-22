98b97fd5494084a92c4ad7187386323b6116581d
aliraad79 <a.ahmadi.k.79@gmail.com>
2023-03-22 12:42:20 +0330

    Insert batch tasks

--- data_aggregator/src/db.rs
+++ data_aggregator/src/db.rs
@@ -4,28 +4,22 @@ use dotenvy::dotenv;
 use std::env;
 
 use crate::schema::tasks;
-use crate::task::{NewTask, Task};
+use crate::task::NewTask;
 
 pub fn establish_connection() -> PgConnection {
     dotenv().ok();
-
-    // println!("{:?}", env::var("DATABASE_URL"));
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
-pub fn add_to_db(task: Task) {
+
+pub fn add_to_db(task: Vec<NewTask>) {
     let mut conn = establish_connection();
 
-    let new_task = NewTask {
-        title: &task.title,
-        description: &task.description,
-    };
-
     diesel::insert_into(tasks::table)
-        .values(&new_task)
+        .values(&task)
         .execute(&mut conn)
         .expect("Error saving new post");
-    println!("Inserted");
+
 }
