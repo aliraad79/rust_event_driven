9dee4f374a13f9906dd9a4478071f839677979ac
aliraad79 <a.ahmadi.k.79@gmail.com>
2023-03-22 12:09:35 +0330

    move task object to seprate file

--- data_aggregator/src/main.rs
+++ data_aggregator/src/main.rs
@@ -1,26 +1,9 @@
 mod db;
 mod rediss;
+mod task;
+mod schema;
 
-use redis::{from_redis_value, ErrorKind, FromRedisValue, RedisResult, Value};
-use serde::{Deserialize, Serialize};
-
-#[derive(Deserialize, Serialize, Debug)]
-pub struct Task {
-    title: String,
-    description: String,
-}
-impl FromRedisValue for Task {
-    fn from_redis_value(v: &Value) -> RedisResult<Self> {
-        let json_str: String = from_redis_value(v)?;
-
-        let result: Self = match serde_json::from_str::<Self>(&json_str) {
-            Ok(v) => v,
-            Err(_err) => return Err((ErrorKind::TypeError, "Parse to JSON Failed").into()),
-        };
-
-        Ok(result)
-    }
-}
+use task::Task;
 
 fn main() {
     // let tasks_string = rediss::get_tasks();
---
+++ data_aggregator/src/task.rs
@@ -0,0 +1,31 @@
+use diesel::prelude::*;
+use redis::{from_redis_value, ErrorKind, FromRedisValue, RedisResult, Value};
+use serde::{Deserialize, Serialize};
+
+use crate::schema::tasks;
+
+#[derive(Deserialize, Serialize, Debug, Queryable)]
+pub struct Task {
+    id: i32,
+    pub title: String,
+    pub description: String,
+}
+impl FromRedisValue for Task {
+    fn from_redis_value(v: &Value) -> RedisResult<Self> {
+        let json_str: String = from_redis_value(v)?;
+
+        let result: Self = match serde_json::from_str::<Self>(&json_str) {
+            Ok(v) => v,
+            Err(_err) => return Err((ErrorKind::TypeError, "Parse to JSON Failed").into()),
+        };
+
+        Ok(result)
+    }
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = tasks)]
+pub struct NewTask<'a> {
+    pub title: &'a str,
+    pub description: &'a str,
+}
