6a7de2f3dfc70cea2d58317f18312b4ac1691f0e
aliraad79 <a.ahmadi.k.79@gmail.com>
2023-03-30 13:44:00 +0330

    Send pure json data in client

--- mock_publisher/src/main.rs
+++ mock_publisher/src/main.rs
@@ -1,17 +1,22 @@
-use serde_json::json;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Task {
+    title: String,
+    description: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     for _ in 0..10 {
-        let data = json!({
-                "title": "test",
-                "description": "Descriptions is long",
-        })
-        .to_string();
+        let task = Task {
+            title: String::from("test"),
+            description: String::from("Descriptions is long"),
+        };
 
         let res = reqwest::Client::new()
-            .post("http://127.0.0.1:8000/task")
-            .body(data)
+            .post("http://127.0.0.1:8080/task")
+            .json(&task)
             .send()
             .await
             .unwrap();
