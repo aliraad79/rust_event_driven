0076ba1f1100ad41a26580ed0703a670ca673d4c
aliraad79 <a.ahmadi.k.79@gmail.com>
2023-04-07 12:51:45 +0330

    Use Threading and sleep instead of celery

--- data_aggregator/src/main.rs
+++ data_aggregator/src/main.rs
@@ -2,16 +2,56 @@ mod db;
 mod rediss;
 mod schema;
 mod task;
-mod celery_tasks;
 
 use dotenvy::dotenv;
+use std::{
+    thread,
+    time::{Duration, Instant},
+};
 
 fn main() {
     dotenv().ok();
-    let last_task = rediss::get_last_n_task(10);
+    let scheduler = thread::spawn(|| {
+        let wait_time = Duration::from_secs(60 * 5);
 
-    match last_task {
+        // Make this an infinite loop
+        // Or some control path to exit the loop
+        loop {
+            let start = Instant::now();
+            eprintln!("Scheduler starting at {:?}", start);
+
+            let thread_a = thread::spawn(heavy_task);
+
+            thread_a.join().expect("Thread A panicked");
+
+            let runtime = start.elapsed();
+
+            if let Some(remaining) = wait_time.checked_sub(runtime) {
+                eprintln!(
+                    "schedule slice has time left over; sleeping for {:?}",
+                    remaining
+                );
+                thread::sleep(remaining);
+            }
+        }
+    });
+
+    scheduler.join().expect("Scheduler panicked");
+}
+
+fn heavy_task() {
+    println!("Running heavy task");
+    let number_of_tasks_in_queue = match rediss::get_number_of_tasks_in_queue() {
+        Ok(n) => n,
+        Err(_err) => {
+            println!("Redis paniced!");
+            return;
+        }
+    };
+
+    println!("Doing {} task in loop", number_of_tasks_in_queue);
+    match rediss::get_last_n_task(number_of_tasks_in_queue) {
         Ok(tasks) => db::add_to_db(tasks),
-        Err(string) => println!("Error {:?}", string),
-    }
+        Err(_err) => println!("Redis paniced!"),
+    };
 }
