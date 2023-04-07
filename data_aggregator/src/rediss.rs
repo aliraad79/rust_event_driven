410c685e68c32707a6ae459daef078eafc7fdf82
aliraad79 <a.ahmadi.k.79@gmail.com>
2023-04-07 12:53:02 +0330

    Use redis url in env file

--- data_aggregator/src/rediss.rs
+++ data_aggregator/src/rediss.rs
@@ -1,15 +1,22 @@
 extern crate redis;
 
-use redis::RedisError;
+use redis::{Commands, RedisError};
 
 use crate::task::NewTask;
 
-pub fn get_last_n_task(n: i32) -> Result<Vec<NewTask>, RedisError> {
-    let client = redis::Client::open("redis://127.0.0.1:6379").unwrap();
+pub fn get_last_n_task(n: i64) -> Result<Vec<NewTask>, RedisError> {
+    let client = redis::Client::open(std::env::var("REDIS_URL").unwrap()).unwrap();
     let mut conn = client.get_connection().unwrap();
 
     redis::cmd("RPOP")
         .arg("tasks")
         .arg(n)
         .query::<Vec<NewTask>>(&mut conn)
+}
+
+pub fn get_number_of_tasks_in_queue() -> Result<i64, RedisError> {
+    let client = redis::Client::open(std::env::var("REDIS_URL").unwrap()).unwrap();
+    let mut conn = client.get_connection().unwrap();
+
+    conn.llen("tasks")
 }
