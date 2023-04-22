9506f30a65f77048ec6d7446bb1f3a160d96f701
aliraad79 <a.ahmadi.k.79@gmail.com>
2023-04-22 15:40:43 +0330

    Dockerize collector

--- collector/src/main.rs
+++ collector/src/main.rs
@@ -43,7 +43,7 @@ async fn main() -> std::io::Result<()> {
             .service(index)
             .service(new_task)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
